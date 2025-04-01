6df90f957b1b7b09aa61647bbf519a1c7fe0436f
SaiSuraj <saisuraj0105@gmail.com>
2025-04-02 03:18:05 +0530

    02/04/2025 added categoryRepository and ExpenseRepository

---
+++ src/main/java/com/expense/tracker/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.expense.tracker.repository;
+
+import com.expense.tracker.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+    Optional<Category> findByName(String name);
+}
